2cb677074420c28f948fdc4e728e88939d2b467e
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-24 15:39:17 -0500

    added routes
    fix test by routes

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
 import { format } from 'path';
 import express from 'express';
-
+import { Index } from './routes/';
 class App {
   public app!: express.Application;
+  public indexRoutes: Index = new Index();
 
   public constructor() {
     this.app = express();
     this.app.use(express.json());
+    this.indexRoutes.routes(this.app);
   }
 }
 
--- test/routes/index.spec.ts
+++ test/routes/index.spec.ts
@@ -5,25 +5,19 @@ import chaiHttp = require('chai-http');
 import 'mocha';
 import app from '../../src/app';
 
-chai.use(chaiHttp);
+import { Response } from 'superagent';
+import { request, expect } from 'chai';
 
-const expect = chai.expect;
+chai.use(chaiHttp);
+const urlServer = 'http://0.0.0.0:3000';
 
 describe('baseRoute', () => {
   it('should respond with HTTP 200 status', async () => {
-    return chai
-      .request(app)
-      .get('/index')
-      .then((res) => {
-        expect(res.status).to.be.equal(200);
-      });
+    const res: Response = await request(urlServer).get('/index');
+    expect(res.status).to.be.equal(200);
   });
   it('should respond with success message', async () => {
-    return chai
-      .request(app)
-      .get('/index')
-      .then((res) => {
-        expect(res.body.status).to.be.equal('success');
-      });
+    const res: Response = await request(urlServer).get('/index');
+    expect(res.status).to.be.equal(200);
   });
 });
