f39cb0e2df5452ebc99e259088d925e2acb4ccbd
Carlos Ramirez <carlos@extrasistemas.com>
2020-09-24 14:10:24 -0500

    adde server.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
+import { format } from 'path';
+import express from 'express';
+
 class App {
-  public app: any;
+  public app!: express.Application;
+
+  public constructor() {
+    this.app = express();
+    this.app.use(express.json());
+  }
 }
 
 export default new App().app;
